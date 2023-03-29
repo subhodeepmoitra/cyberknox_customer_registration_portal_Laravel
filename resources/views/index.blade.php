aec1685688edab108cb47c8a6db1a867346c7e43
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2023-03-29 19:37:57 +0530

    Jai Jagannath

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,8 +17,10 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #e4cdaf;">
         <div class="container-fluid">
+            <img src="{{asset('index/index_img1.jpg')}}" width="100" height="50" class="d-inline-block align-top" alt="">
             <a class="navbar-brand" href="#">Cyberknox</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -28,10 +30,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('/home')}}">Login</a>
+                        <a class="nav-link" href="{{ url('/home') }}">Login</a>
                     </li>
 
                     <li class="nav-item dropdown">
@@ -47,9 +49,6 @@
                             </li>
                             <li><a class="dropdown-item" href="#">Something else here</a></li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Login</a>
                     </li>
                 </ul>
                 {{-- <form class="d-flex">
